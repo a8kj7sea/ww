946b7ab530e7f6c74c21c07709f257d7fd10116d
a8kj7sea <sanhakns24@gmail.com>
2024-10-01 03:53:01 -0700

    Finish listeners implemention

---
+++ src/main/java/me/a8kj/ww/internal/listeners/OtherListeners.java
@@ -0,0 +1,128 @@
+package me.a8kj.ww.internal.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.EntityTeleportEvent;
+
+import io.lumine.mythic.core.mobs.ActiveMob;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.a8kj.ww.internal.configuration.enums.SettingsPathIdentifiers;
+import me.a8kj.ww.internal.configuration.files.SettingsFile;
+import me.a8kj.ww.internal.configuration.retrievers.SettingsRetriever;
+import me.a8kj.ww.parent.entity.plugin.PluginProvider;
+import me.a8kj.ww.parent.utils.WorldGuardUtils;
+
+/**
+ * Listener for handling various entity events in the game, including spawning
+ * and teleporting.
+ */
+@RequiredArgsConstructor
+@Getter
+public class OtherListeners implements Listener {
+    private final PluginProvider pluginProvider;
+
+    /**
+     * Handles the CreatureSpawnEvent to manage the spawning of creatures.
+     *
+     * @param event the event triggered when a creature spawns
+     */
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        Entity entity = event.getEntity();
+        String regionName = getRegionName();
+        String mobName = getMobName();
+
+        if (isAllowedToSpawn(entity, mobName)) {
+            return; // Allow players and specific MythicMob to spawn
+        }
+
+        if (WorldGuardUtils.isInRegion(entity, regionName)) {
+            event.setCancelled(true);
+        }
+    }
+
+    /**
+     * Handles the EntityTeleportEvent to restrict teleportation of specific
+     * entities.
+     *
+     * @param event the event triggered when an entity teleports
+     */
+    @EventHandler
+    public void onEntityTeleport(EntityTeleportEvent event) {
+        Entity entity = event.getEntity();
+
+        if (entity == null || entity.getName() == null || entity.getCustomName() == null) {
+            return;
+        }
+
+        if (!entity.getName().equalsIgnoreCase(getMobName())) {
+            return;
+        }
+
+        if (!WorldGuardUtils.isInRegion(event.getTo(), getRegionName())) {
+            event.setCancelled(true);
+        }
+    }
+
+    /**
+     * Handles the EntitySpawnEvent to manage the spawning of general entities.
+     *
+     * @param event the event triggered when an entity spawns
+     */
+    @EventHandler
+    public void onEntitySpawnEvent(EntitySpawnEvent event) {
+        Entity entity = event.getEntity();
+
+        if (entity == null || entity.getName() == null || entity.getCustomName() == null) {
+            return;
+        }
+
+        if (isAllowedToSpawn(entity, getMobName())) {
+            return;
+        }
+
+        if (WorldGuardUtils.isInRegion(entity, getRegionName())) {
+            event.setCancelled(true);
+        }
+    }
+
+    /**
+     * Checks if the entity is allowed to spawn based on its type and name.
+     *
+     * @param entity  the entity being spawned
+     * @param mobName the name of the mob to check against
+     * @return true if the entity is allowed to spawn, false otherwise
+     */
+    private boolean isAllowedToSpawn(Entity entity, String mobName) {
+        return entity instanceof Player || (entity instanceof ActiveMob && entity.getName().equalsIgnoreCase(mobName));
+    }
+
+    /**
+     * Retrieves the mob name from the configuration.
+     *
+     * @return the name of the mob
+     */
+    private String getMobName() {
+        SettingsFile settingsFile = (SettingsFile) pluginProvider.getConfigurationManager()
+                .getConfiguration("settings");
+        SettingsRetriever settingsRetriever = new SettingsRetriever(settingsFile.getYamConfiguration());
+        return settingsRetriever.getString(SettingsPathIdentifiers.MOB_NAME);
+    }
+
+    /**
+     * Retrieves the region name from the configuration.
+     *
+     * @return the name of the region
+     */
+    private String getRegionName() {
+        SettingsFile settingsFile = (SettingsFile) pluginProvider.getConfigurationManager()
+                .getConfiguration("settings");
+        SettingsRetriever settingsRetriever = new SettingsRetriever(settingsFile.getYamConfiguration());
+        return settingsRetriever.getString(SettingsPathIdentifiers.REGION_NAME);
+    }
+}
--- src/main/java/me/a8kj/ww/internal/listeners/mob/EntityDamageByEntityListener.java
+++ src/main/java/me/a8kj/ww/internal/listeners/mob/EntityDamageByEntityListener.java
@@ -1,5 +1,152 @@
 package me.a8kj.ww.internal.listeners.mob;
 
-public class EntityDamageByEntityListener {
-    
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import io.lumine.mythic.core.mobs.ActiveMob;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.a8kj.ww.internal.configuration.enums.MessagePathIdentifiers;
+import me.a8kj.ww.internal.configuration.enums.SettingsPathIdentifiers;
+import me.a8kj.ww.internal.configuration.files.MessagesFile;
+import me.a8kj.ww.internal.configuration.files.SettingsFile;
+import me.a8kj.ww.internal.configuration.retrievers.SettingsRetriever;
+import me.a8kj.ww.internal.configuration.retrievers.messages.MessageRetriever;
+import me.a8kj.ww.parent.entity.plugin.PluginProvider;
+import me.a8kj.ww.parent.utils.PlayerUtils;
+import me.a8kj.ww.parent.utils.WorldGuardUtils;
+
+/**
+ * Listener for handling entity damage events between players and mobs.
+ */
+@RequiredArgsConstructor
+@Getter
+public class EntityDamageByEntityListener implements Listener {
+
+    private final PluginProvider pluginProvider;
+
+    /**
+     * Handles the EntityDamageByEntityEvent to manage damage interactions.
+     *
+     * @param event the event triggered when an entity is damaged by another entity
+     */
+    @EventHandler
+    public void onDamageByEntity(EntityDamageByEntityEvent event) {
+        if (isInvalidEvent(event))
+            return;
+
+        String mobName = getMobName();
+        String regionName = getRegionName();
+
+        if (event.getEntity() instanceof Player victim) {
+            handlePlayerDamage(event, victim, mobName, regionName);
+        } else if (event.getEntity() instanceof ActiveMob || event.getEntity() instanceof Mob) {
+            handleMobDamage(event, event.getEntity(), mobName, regionName);
+        }
+    }
+
+    /**
+     * Checks if the event is valid for processing.
+     *
+     * @param event the event to check
+     * @return true if the event is invalid, false otherwise
+     */
+    private boolean isInvalidEvent(EntityDamageByEntityEvent event) {
+        return event.getDamager() == null || event.getEntity() == null || event.getDamageSource() == null
+                || event.isCancelled() || event.getDamage() <= 0.0;
+    }
+
+    /**
+     * Retrieves the mob name from the configuration.
+     *
+     * @return the name of the mob
+     */
+    private String getMobName() {
+        SettingsFile settingsFile = (SettingsFile) pluginProvider.getConfigurationManager()
+                .getConfiguration("settings");
+        SettingsRetriever settingsRetriever = new SettingsRetriever(settingsFile.getYamConfiguration());
+        return settingsRetriever.getString(SettingsPathIdentifiers.MOB_NAME);
+    }
+
+    /**
+     * Retrieves the region name from the configuration.
+     *
+     * @return the name of the region
+     */
+    private String getRegionName() {
+        SettingsFile settingsFile = (SettingsFile) pluginProvider.getConfigurationManager()
+                .getConfiguration("settings");
+        SettingsRetriever settingsRetriever = new SettingsRetriever(settingsFile.getYamConfiguration());
+        return settingsRetriever.getString(SettingsPathIdentifiers.REGION_NAME);
+    }
+
+    /**
+     * Handles damage dealt to a player by a mob.
+     *
+     * @param event      the damage event
+     * @param victim     the player being damaged
+     * @param mobName    the name of the mob
+     * @param regionName the name of the region
+     */
+    private void handlePlayerDamage(EntityDamageByEntityEvent event, Player victim, String mobName, String regionName) {
+        Entity damager = event.getDamageSource().getCausingEntity();
+
+        if (!(damager instanceof ActiveMob || damager instanceof Mob) || damager.getName() == null)
+            return;
+        if (!damager.getName().equalsIgnoreCase(mobName))
+            return;
+
+        if (!WorldGuardUtils.isInRegion(victim, regionName)) {
+            event.setCancelled(true);
+        }
+
+        float health = (float) ((Damageable) damager).getHealth();
+        String healthFormatted = String.format("%.1f", health);
+        PlayerUtils.broadcastMessage(
+                getMessage(MessagePathIdentifiers.GAME_LOGIC_COMBAT_INGAME_ATTACKED_BROADCAST, healthFormatted));
+    }
+
+    /**
+     * Handles damage dealt to a mob by a player.
+     *
+     * @param event      the damage event
+     * @param entity     the mob being damaged
+     * @param mobName    the name of the mob
+     * @param regionName the name of the region
+     */
+    private void handleMobDamage(EntityDamageByEntityEvent event, Entity entity, String mobName,
+            String regionName) {
+        if (entity.getName() == null || !entity.getName().equalsIgnoreCase(mobName))
+            return;
+
+        if (event.getDamageSource().getCausingEntity() instanceof Player damager) {
+            if (!WorldGuardUtils.isInRegion(damager, regionName)) {
+                damager.sendMessage(getMessage(MessagePathIdentifiers.GAME_LOGIC_COMBAT_INGAME_NOT_ALLOWED_DAMAGE));
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    /**
+     * Retrieves a message from the configuration, replacing placeholders as needed.
+     *
+     * @param pathIdentifier the message path identifier
+     * @param replacements   the replacements for placeholders in the message
+     * @return the formatted message
+     */
+    private String getMessage(MessagePathIdentifiers pathIdentifier, String... replacements) {
+        MessagesFile messagesFile = (MessagesFile) pluginProvider.getConfigurationManager()
+                .getConfiguration("messages");
+        MessageRetriever messageRetriever = new MessageRetriever(messagesFile.getYamConfiguration());
+        String message = messageRetriever.getMessage(pathIdentifier);
+        for (String replacement : replacements) {
+            message = message.replace("%mob_hp%", replacement);
+        }
+        return message;
+    }
 }
